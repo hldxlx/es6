2c43afe73759dac99a78af6291f1b3c48a8240e2
斌斌斌 <1587443838@qq.com>
2019-07-14 20:37:55 +0800

    JavaScript_简单框架封装

--- src/demo/js/myJq.js
+++ src/demo/js/myJq.js
@@ -13,17 +13,47 @@
     jQuery.prototype = {
        init:function(element){
         //$("#box") 选择节点的初始化方法
-           var dom = document.querySelectorAll(element);
+           var dom = null;
+           if(typeof element != 'string'){
+               dom = [element];
+           }else{
+                dom = document.querySelectorAll(element);
+           }
            return markAll(dom,this);
        },
-        css:function(){
-
+        css:function(attr,val){
+           for(var i=0;i<this.length;i++){
+               this[i].style[attr] = val;
+           }
+            return this;
         },
-        html:function(){
-
+        html:function(val){
+            for(var i=0;i<this.length;i++){
+                this[i].innerHTML = val;
+            }
+            return this;
+        },
+        hover:function (over,out) {
+            for(var i=0;i<this.length;i++){
+                this[i].onmouseover = over;
+                this[i].onmouseout = out;
+            }
+            return this;
+        },
+        first:function () {
+            return $(this[0])
         }
     }
+    jQuery.prototype.init.prototype = jQuery.prototype;
    e.jQuery = e.$ = jQuery;
 
 })(window);
+console.log(
+    $("li").first().css('background','red').html("改变了")
+    .hover(function () {
+        alert(1);
+    },function () {
+        alert(2);
+    })
+)
 
