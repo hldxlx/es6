04d386decb36f497b619a61a2448d4e38c054326
斌斌斌 <1587443838@qq.com>
2019-07-14 20:16:24 +0800

    简单封装

--- src/demo/js/myJq.js
+++ src/demo/js/myJq.js
@@ -1,7 +1,29 @@
 ;(function (e) {
-   var jQuery = function(){
-        return '4684'
+   var jQuery = function(element){
+        return new jQuery.prototype.init(element);
    }
+   function markAll(dom,that) {
+       var that = that || [];
+       for(var i=0;i<dom.length;i++){
+           that[i] = dom[i];
+       }
+       that.length = dom.length;
+       return that;
+   }
+    jQuery.prototype = {
+       init:function(element){
+        //$("#box") 选择节点的初始化方法
+           var dom = document.querySelectorAll(element);
+           return markAll(dom,this);
+       },
+        css:function(){
+
+        },
+        html:function(){
+
+        }
+    }
    e.jQuery = e.$ = jQuery;
 
 })(window);
+
