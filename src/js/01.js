0c38d476d0fd7e81c074750a2bd6e0afeba65784
斌斌斌 <1587443838@qq.com>
2019-08-04 19:19:35 +0800

    class 继承

--- src/js/01.js
+++ src/js/01.js
@@ -1,7 +1,31 @@
-let str = '自动刷新你好binsom';
-console.log(str);
-let arr = ['a','b','c'];
-
-for (let n of arr.values()){
-    console.log(n)
+// let str = '自动刷新你好binsom';
+// console.log(str);
+// let arr = ['a','b','c'];
+//
+// for (let n of arr.values()){
+//     console.log(n)
+// }
+class A {
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+        this.x = 1;
+    }
+    print() {
+        console.log(this.num);
+    }
 }
+
+class B extends A {
+    constructor(name,age,num) {
+        super(name,age);
+        this.x = 2;
+        this.num = num
+    }
+    m() {
+        super.print();
+    }
+}
+
+let b = new B('ww',45,997777);
+b.m();
