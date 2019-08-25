37eb6fc89bc6d612310dfc85679c1d21d0d8eeb4
斌斌斌 <1587443838@qq.com>
2019-08-25 17:29:38 +0800

    dataview

--- src/js/subarray.js
+++ src/js/subarray.js
@@ -3,3 +3,13 @@
 // console.log(tarr2);
 // console.log(tarr2.buffer === tarr2.buffer);
 
+
+//? 创建一个dataview就可以对很多buffer进行设置和读取
+let buffer = new ArrayBuffer(10);
+let dataview = new DataView(buffer);
+dataview.setFloat32(2,18.9);
+dataview.setInt32(6,2000);
+dataview.setInt16(0,90);
+
+let int8Arr = new Int8Array(buffer);
+console.log(int8Arr);
